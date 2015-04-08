6929df808b874feeaea51657cadba38be9733bc9
Christophe Villeger <christophe.villeger@alcatel-lucent.com>
2015-04-08 19:27:15 +0200

    Add deconnect link

--- index.php
+++ index.php
@@ -42,7 +42,9 @@
         $request_user = new FacebookRequest($session, "GET", "/me");
         $request_user_executed = $request_user->execute();
         $user = $request_user_executed->getGraphObject(\Facebook\GraphUser::className());
+        $logoutUrl = $helper->getLogoutUrl($session, WEBURL);
         echo $user->getName();
+        echo "<a href='".$logoutUrl."'>Déconnection</a>";
 
     } else {
         $loginUrl = $helper->getLoginUrl();
