903c7a2380ec0c74844d6f1a2627780eeb281d55
Baptiste Linel <baptiste.linel@gmail.com>
2015-06-17 14:13:48 +0200

    Fix design problem : no bottom margin

--- web/include/footer.php
+++ web/include/footer.php
@@ -1,20 +1,22 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="public/js/jquery.simplemodal.1.4.4.min.js"></script>
-<script src="public/js/cookiebanner.js"></script>
-<script src="public/js/main.js"></script>
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId      : '554865951320925',
-            xfbml      : true,
-            version    : 'v2.3'
-        });
-    };
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
+<footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="public/js/jquery.simplemodal.1.4.4.min.js"></script>
+    <script src="public/js/cookiebanner.js"></script>
+    <script src="public/js/main.js"></script>
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '554865951320925',
+                xfbml      : true,
+                version    : 'v2.3'
+            });
+        };
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+</footer>
